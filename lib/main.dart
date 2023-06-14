819e07026cdbbceda0b0379199d17b6e0a717024
Moaz <moazomran20@gmail.com>
2023-06-15 01:15:34 +0300

    phrases category added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:tokyo_app/layouts/home_screen.dart';
 import 'package:tokyo_app/screens/colors_screen.dart';
 import 'package:tokyo_app/screens/family_members.dart';
 import 'package:tokyo_app/screens/numbers_screen.dart';
+import 'package:tokyo_app/screens/phrases_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
         NumbersScreen.routeName: (context) => NumbersScreen(),
         FamilyMembersScreen.routeName:(context)=>FamilyMembersScreen(),
         ColorsScreen.routeName:(context)=>ColorsScreen(),
+        PhrasesScreen.routeName:(context)=>PhrasesScreen(),
       },
       initialRoute: HomeScreen.routeName,
     );
---
+++ lib/screens/widgets/phrases_item.dart
@@ -0,0 +1,51 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:tokyo_app/models/phrases_model.dart';
+
+class PhrasesItem extends StatelessWidget {
+
+  PhrasesModel phrasesModel;
+
+
+  PhrasesItem(this.phrasesModel);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      color: const Color(0xFFF8942D),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                 phrasesModel.phraseTokyo,
+                  style: const TextStyle(color: Colors.white, fontSize: 15,fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  phrasesModel.phraseEnglish,
+                  style: const TextStyle(color: Colors.white, fontSize: 15),
+                )
+              ],
+            ),
+          ),
+          const Spacer(),
+          IconButton(
+              onPressed: () {
+                var player = AudioCache(prefix: "assets/sounds/phrases/");
+                player.play(phrasesModel.phraseAudioName);
+              },
+              icon: const Icon(
+                Icons.play_arrow,
+                size: 30,
+                color: Colors.white,
+              ))
+        ],
+      ),
+    );
+  }
+}
